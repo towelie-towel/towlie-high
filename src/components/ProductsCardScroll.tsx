7ee009b9f21ffccaedd27b63e92f62dd93e38d12
ackerman <julio.sergio2709@gmail.com>
2023-12-08 18:13:07 -0500

    fix: :zap:

--- src/components/ProductsCardScroll.tsx
+++ src/components/ProductsCardScroll.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import { useCart } from "~/context/ShoppingCart";
